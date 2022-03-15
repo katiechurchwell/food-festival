eaf1d6e8f92f262461a97c6c29f62b387ec58149
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-14 21:01:44 -0700

    webpack output and popper fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ const webpack = require("webpack");
 module.exports = {
   entry: './assets/js/script.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js',
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
   },
   plugins: [
     new webpack.ProvidePlugin({
