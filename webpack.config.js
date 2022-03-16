b7e02abfcf69d01baa82fc24855d4b41d4bedb7d
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-16 15:38:59 -0700

    code split - seperate concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
-const webpack = require("webpack");
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './assets/js/script.js',
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    path: path.join(__dirname + '/dist'),
+    filename: 'main.bundle.js',
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
   ],
   mode: 'development',
 };
