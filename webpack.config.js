4c98c5d962f401a9b86ab380fc638c65bed97828
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2022-03-16 19:08:57 -0700

    multiple entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js',
+  },
   output: {
     path: path.join(__dirname + '/dist'),
-    filename: 'main.bundle.js',
+    filename: "[name].bundle.js", //The name of each attribute in the entry object will be used in place of [name] in each bundle.js file that is created
   },
   plugins: [
     new webpack.ProvidePlugin({
@@ -14,8 +20,8 @@ module.exports = {
       jQuery: 'jquery',
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-    })
+      analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+    }),
   ],
   mode: 'development',
 };
